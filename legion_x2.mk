a73637e78fbf79336401d593b2ddcbd3ed040e0e
CrisBal <crisbaladim@yahoo.gr>
2021-09-11 11:52:46 +0300

    x2: Switch to Pixel 5a september fp

--- legion_x2.mk
+++ legion_x2.mk
@@ -47,6 +47,6 @@ PRODUCT_GMS_CLIENTID_BASE := android-leeco
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE=le_x2 \
     PRODUCT_NAME=LeMax2_WW \
-    PRIVATE_BUILD_DESC="redfin-user 11 RQ3A.210805.001.A1 7474174 release-keys"
+    PRIVATE_BUILD_DESC="barbet-user 11 RD2A.210905.002 7513089 release-keys"
 
-BUILD_FINGERPRINT := google/redfin/redfin:11/RQ3A.210805.001.A1/7474174:user/release-keys
+BUILD_FINGERPRINT := google/barbet/barbet:11/RD2A.210905.002/7513089:user/release-keys
